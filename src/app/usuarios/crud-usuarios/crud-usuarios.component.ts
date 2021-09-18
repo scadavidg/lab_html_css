1f96bedaa3cbbc2fe3fef35acbaf58d9b08c48d5
Santiago Cadavid Gutierrez <31990704+scadavidg@users.noreply.github.com>
2021-09-17 21:38:32 -0500

    Develop (#4)
    * added bootstraps,font awesome icons
    
    * listar usuarios
    
    * create, edit user forms
    
    Co-authored-by: scadavidg <scadavid@unal.edu.co>

---
+++ src/app/usuarios/crud-usuarios/crud-usuarios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-usuarios',
+  templateUrl: './crud-usuarios.component.html',
+  styleUrls: ['./crud-usuarios.component.scss']
+})
+export class CrudUsuariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
